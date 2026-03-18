29fd0c0f0ac33d6b8b4425f30402e7333b818716
juju <juanjosecu234Qgmail.com>
2026-03-18 09:11:41 -0500

    Cambio de variables, abreviaturas cambiadas por nombres de variables mas entendibles

--- src/core/logica.py
+++ src/core/logica.py
@@ -2,61 +2,59 @@ class ErrorMetaMayorACero(Exception):
     """La meta debe ser mayor que 0"""
     pass
 
-
 class ErrorPlazoMayorACero(Exception):
     """El plazo debe ser mayor que 0"""
     pass
-
 
 class ErrorAbonoExtraMenorACero(Exception):
     """El abono extra no puede ser menor que 0"""
     pass
 
-
 class ErrorAbonoSuperaMeta(Exception):
     """El abono extra no puede superar la meta"""
     pass
-
 
 class ErrorMesExtraFueraDelRango(Exception):
     """El mes del abono no puede superar el plazo"""
     pass
 
-
 class AhorroProgramado:
-    def __init__(self, meta, plazo, extra=0, mes_extra=0):
+    def __init__(self, meta, plazo, abono_extra=0, mes_abono_extra=0):
         self.meta = meta
         self.plazo = plazo
-        self.extra = extra
-        self.mes_extra = mes_extra
-        self.tasa = 0.0075
+        self.abono_extra = abono_extra
+        self.mes_abono_extra = mes_abono_extra
+        self.tasa_interes = 0.0075
 
     def calcular_ahorro(self):
-        # Validaciones
+        # Validaciones de integridad de datos
         if self.meta <= 0:
             raise ErrorMetaMayorACero()
 
         if self.plazo <= 0:
             raise ErrorPlazoMayorACero()
 
-        if self.extra < 0:
+        if self.abono_extra < 0:
             raise ErrorAbonoExtraMenorACero()
 
-        if self.extra > self.meta:
+        if self.abono_extra > self.meta:
             raise ErrorAbonoSuperaMeta()
 
-        if self.mes_extra > self.plazo:
+        if self.mes_abono_extra > self.plazo:
             raise ErrorMesExtraFueraDelRango()
 
-        # Cálculo
-        i = self.tasa
-        n = self.plazo
-        k = self.mes_extra
+       
+        interes = self.tasa_interes
+        total_periodos = self.plazo
+        momento_abono_extra = self.mes_abono_extra
 
-        fv_extra = self.extra * ((1 + i) ** (n - k)) if k > 0 else 0
-        factor = ((1 + i) ** n - 1) / i
-        cuota = (self.meta - fv_extra) / factor
+        if momento_abono_extra > 0:
+            periodos_restantes = total_periodos - momento_abono_extra
+            valor_futuro_abono_extra = self.abono_extra * ((1 + interes) ** periodos_restantes)
+        else:
+            valor_futuro_abono_extra = 0
 
-        return round(cuota, 2)
+        factor_acumulacion = ((1 + interes) ** total_periodos - 1) / interes
+        cuota_mensual = (self.meta - valor_futuro_abono_extra) / factor_acumulacion
 
-
+        return round(cuota_mensual, 2)
